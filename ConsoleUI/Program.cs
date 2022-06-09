be78951d09938c6edf77db6817bbe75d2c5621f8
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-06-10 02:31:49 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// ben InMemoryProductDal ile çalışacağım demektir
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
