837e2b3666dacec4803e98c8791e3bf13e71cce5
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-06-21 16:03:19 +0300

    6 Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,11 +11,36 @@ namespace ConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {// ben InMemoryProductDal ile çalışacağım demektir
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+        {
+            ProductTest();
+
+            //CategoryTest();
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+
+            }
+        }
+
+        private static void ProductTest()
+        {
+            // ben InMemoryProductDal, EfProductDal ile çalışacağım demektir
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            //foreach (var product in productManager.GetByUnitPrice(40, 100))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
         }
     }
