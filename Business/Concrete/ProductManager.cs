9ceeb5f5956d770a57beecc72cb16b93ceae3dc6
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-07-03 13:27:23 +0300

    13 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,7 +37,7 @@ namespace Business.Concrete
         {
             //iş kodları olacak
             //yetkisi var mı ?
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
