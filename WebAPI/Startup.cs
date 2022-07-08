5525c39c205956ff4d3e508b90ad9a111c1d85fa
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-07-08 17:40:09 +0300

    17 Şubat Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,8 +33,9 @@ namespace WebAPI
             //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container 
             //AOP -- 
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
